69096048c341f7382332697346d8b71850293252
Graham Poulter <>
2009-06-22 16:02:52 +0200

    Use of sys.path for unit tests

--- dedupe/tests/__init__.py
+++ dedupe/tests/__init__.py
@@ -4,9 +4,14 @@
 import sys, re, os, unittest
 
 def suite():
-    """Create test suite by listing the modules in this directory"""
+    """Create test suite from the modules in this directory.  First puts the
+    parent dedupe package into the sys.path """
+    tests_dir = os.path.dirname(os.path.abspath(__file__))
+    dedupe_dir = os.path.dirname(tests_dir)
+    dedupe_pkg = os.path.dirname(dedupe_dir)
+    sys.path.insert(0, dedupe_pkg)
     testmods = [ 'dedupe.tests.' + re.match(r'(.+?)\.py$', f).group(1) 
-                 for f in os.listdir(os.path.dirname(os.path.abspath(__file__)))
+                 for f in os.listdir(tests_dir)
                  if re.match(r'test_(.+?)\.py$', f) is not None ]
     print "Testing Modules: %s" % ", ".join(testmods)
     return unittest.defaultTestLoader.loadTestsFromNames(testmods)
