75bd1e3a5e956f6accf08b1703f8d038c920db3c
Graham Poulter <>
2009-06-25 17:26:48 +0200

    Added setup.py, updated README and INSTALL

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#import ez_setup
+#ez_setup.use_setuptools()
+from setuptools import setup
+
+setup(
+    name='dedupe',
+    version='1.0',
+    zip_safe=True,
+
+    author='Graham Poulter',
+    author_url='http://www.grahampoulter.com',
+    license='GPL',
+    test_suite="dedupe.tests",
+
+    url='http://launchpad.net/pydedupe',
+    description="Library for identifying similar pairs of records within or between sets for records."""
+)
