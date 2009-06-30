ab00b62edb6c1c89715aa4799dc618144c38a212
Graham Poulter <>
2009-06-30 16:45:22 +0200

    fixed odict->OrderedDict in ordereddict.py

--- dedupe/compat/ordereddict.py
+++ dedupe/compat/ordereddict.py
@@ -1,6 +1,6 @@
 """
-odict
-~~~~~
+OrderedDict
+~~~~~~~~~~~
 
 Source: http://dev.pocoo.org/hg/sandbox/raw-file/tip/odict.py
 
@@ -50,26 +50,26 @@ What happens if an existing key is reassigned?
     The key is *not* moved.  This is consitent with existing
     implementations and can be changed by a subclass very easily::
 
-        class movingodict(odict):
+        class movingodict(OrderedDict):
             def __setitem__(self, key, value):
                 self.pop(key, None)
-                odict.__setitem__(self, key, value)
+                OrderedDict.__setitem__(self, key, value)
 
     Moving keys to the end of a ordered dict on reassignment is not
     very useful for most applications.
 
 Does it mean the dict keys are sorted by a sort expression?
 
-    That's not the case.  The odict only guarantees that there is an order
+    That's not the case.  The OrderedDict only guarantees that there is an order
     and that newly inserted keys are inserted at the end of the dict.  If
     you want to sort it you can do so, but newly added keys are again added
     at the end of the dict.
 
-I initializes the odict with a dict literal but the keys are not
+I initializes the OrderedDict with a dict literal but the keys are not
 ordered like they should!
 
     Dict literals in Python generate dict objects and as such the order of
-    their items is not guaranteed.  Before they are passed to the odict
+    their items is not guaranteed.  Before they are passed to the OrderedDict
     constructor they are already unordered.
 
 What happens if keys appear multiple times in the list passed to the
@@ -79,16 +79,16 @@ constructor?
     has the side-effect that the position of the first key is used because
     the key is actually overwritten:
 
-    >>> odict([('a', 1), ('b', 2), ('a', 3)])
-    odict.odict([('a', 3), ('b', 2)])
+    >>> OrderedDict([('a', 1), ('b', 2), ('a', 3)])
+    OrderedDict([('a', 3), ('b', 2)])
 
     This behavor is consistent with existing implementation in Python
     and the PHP array and the hashmap in Ruby 1.9.
 
-This odict doesn't scale!
+This OrderedDict doesn't scale!
 
     Yes it doesn't.  The delitem operation is O(n).  This is file is a
-    mockup of a real odict that could be implemented for collections
+    mockup of a real OrderedDict that could be implemented for collections
     based on an linked list.
 
 Why is there no .insert()?
@@ -96,13 +96,13 @@ Why is there no .insert()?
     There are few situations where you really want to insert a key at
     an specified index.  To now make the API too complex the proposed
     solution for this situation is creating a list of items, manipulating
-    that and converting it back into an odict:
+    that and converting it back into an OrderedDict:
 
-    >>> d = odict([('a', 42), ('b', 23), ('c', 19)])
+    >>> d = OrderedDict([('a', 42), ('b', 23), ('c', 19)])
     >>> l = d.items()
     >>> l.insert(1, ('x', 0))
-    >>> odict(l)
-    odict.odict([('a', 42), ('x', 0), ('b', 23), ('c', 19)])
+    >>> OrderedDict(l)
+    OrderedDictedDict([('a', 42), ('x', 0), ('b', 23), ('c', 19)])
 """
 from itertools import izip, imap
 from copy import deepcopy
@@ -123,31 +123,31 @@ class OrderedDict(dict):
     The constructor and `update()` both accept iterables of tuples as well as
     mappings:
 
-    >>> d = odict([('a', 'b'), ('c', 'd')])
+    >>> d = OrderedDict([('a', 'b'), ('c', 'd')])
     >>> d.update({'foo': 'bar'})
     >>> d
-    odict.odict([('a', 'b'), ('c', 'd'), ('foo', 'bar')])
+    OrderedDict([('a', 'b'), ('c', 'd'), ('foo', 'bar')])
     
     Keep in mind that when updating from dict-literals the order is not
     preserved as these dicts are unsorted!
 
-    You can copy an odict like a dict by using the constructor, `copy.copy`
+    You can copy an OrderedDict like a dict by using the constructor, `copy.copy`
     or the `copy` method and make deep copies with `copy.deepcopy`:
 
     >>> from copy import copy, deepcopy
     >>> copy(d)
-    odict.odict([('a', 'b'), ('c', 'd'), ('foo', 'bar')])
+    OrderedDict([('a', 'b'), ('c', 'd'), ('foo', 'bar')])
     >>> d.copy()
-    odict.odict([('a', 'b'), ('c', 'd'), ('foo', 'bar')])
-    >>> odict(d)
-    odict.odict([('a', 'b'), ('c', 'd'), ('foo', 'bar')])
+    OrderedDict([('a', 'b'), ('c', 'd'), ('foo', 'bar')])
+    >>> OrderedDict(d)
+    OrderedDict([('a', 'b'), ('c', 'd'), ('foo', 'bar')])
     >>> d['spam'] = []
     >>> d2 = deepcopy(d)
     >>> d2['spam'].append('eggs')
     >>> d
-    odict.odict([('a', 'b'), ('c', 'd'), ('foo', 'bar'), ('spam', [])])
+    OrderedDict([('a', 'b'), ('c', 'd'), ('foo', 'bar'), ('spam', [])])
     >>> d2
-    odict.odict([('a', 'b'), ('c', 'd'), ('foo', 'bar'), ('spam', ['eggs'])])
+    OrderedDict([('a', 'b'), ('c', 'd'), ('foo', 'bar'), ('spam', ['eggs'])])
 
     All iteration methods as well as `keys`, `values` and `items` return
     the values ordered by the the time the key-value pair is inserted:
@@ -171,17 +171,17 @@ class OrderedDict(dict):
     >>> d.byindex(2)
     ('foo', 'bar')
 
-    You can reverse the odict as well:
+    You can reverse the OrderedDict as well:
 
     >>> d.reverse()
     >>> d
-    odict.odict([('spam', []), ('foo', 'bar'), ('c', 'd'), ('a', 'b')])
+    OrderedDict([('spam', []), ('foo', 'bar'), ('c', 'd'), ('a', 'b')])
     
     And sort it like a list:
 
     >>> d.sort(key=lambda x: x[0].lower())
     >>> d
-    odict.odict([('a', 'b'), ('c', 'd'), ('foo', 'bar'), ('spam', [])])
+    OrderedDict([('a', 'b'), ('c', 'd'), ('foo', 'bar'), ('spam', [])])
 
     .. _proposal: http://thread.gmane.org/gmane.comp.python.devel/95316
     .. _ordereddict: http://www.xs4all.nl/~anthon/Python/ordereddict/
@@ -223,7 +223,7 @@ class OrderedDict(dict):
         return reversed(self._keys)
 
     def __eq__(self, other):
-        if isinstance(other, odict):
+        if isinstance(other, OrderedDict):
             if not dict.__eq__(self, other):
                 return False
             return self.items() == other.items()
@@ -233,7 +233,7 @@ class OrderedDict(dict):
         return not self.__eq__(other)
 
     def __cmp__(self, other):
-        if isinstance(other, odict):
+        if isinstance(other, OrderedDict):
             return cmp(self.items(), other.items())
         elif isinstance(other, dict):
             return dict.__cmp__(self, other)
@@ -314,7 +314,7 @@ class OrderedDict(dict):
         self._keys.sort(*args, **kwargs)
 
     def __repr__(self):
-        return 'odict.odict(%r)' % self.items()
+        return 'OrderedDict(%r)' % self.items()
 
     __copy__ = copy
     __iter__ = iterkeys
