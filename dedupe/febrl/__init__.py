8b9899dd9a44dc18d72654724bbd950c5e7b5da9
Graham Poulter <>
2009-07-01 14:38:45 +0200

    Added docstring to __init__.py explaining inclustion of FEBRL modules

---
+++ dedupe/febrl/__init__.py
@@ -0,0 +1,3 @@
+"""FEBRL is the Freely Extensible Biomedical Record Linkage program.  Its
+classification and comparison modules have been included in PyDedupe to
+provide subroutines for comparing strings and classifying vectors."""
