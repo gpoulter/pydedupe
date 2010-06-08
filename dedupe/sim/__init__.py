4a63a06debc0448c389e9b77b60156a4e7d13b52
Graham Poulter <>
2010-06-08 18:30:55 +0200

    Removed getters from sim, renamed ValueSim/ValueSimAvg/ValueSimMax to Field,Average,Maximum

--- dedupe/sim/__init__.py
+++ dedupe/sim/__init__.py
@@ -65,144 +65,70 @@ class Scale:
         if  v >= self.high:
             return 1.0
         return (v-self.low)/(self.high-self.low)
-    
-def fallback(main, backup, row):
-    """Attempt to use main attribute of row, but fall back to a backup attribute
-    if the main one is empty."""
-    val = ""
-    if hasattr(row, main):
-        val = getattr(row, main)
-    if not val.strip() and hasattr(row, backup):
-        val = getattr(row, backup)
-    return val
 
-def getvalue(record, field):
-    """Retrieve value of a field from a record by any means.
-    
-    :type record: :class:`namedtuple` or :class:`tuple`
-    :param record: record from which to retrive field value
-    :type field: :class:`str`, :class:`int`, or function(`R`)
-    :param field: field specifier, to attempt R.field, R[field] and field(R)
-    :rtype: any field value (string, number, or collection of strings)
-    :return: Value of `field` on `record`
-    
-    >>> from collections import namedtuple
-    >>> from dedupe import sim
-    >>> sim.getvalue(namedtuple('Record','A B')(1,2), 'A')
-    1
-    >>> sim.getvalue(['a','b'], 0)
-    'a'
-    >>> sim.getvalue({'A':'B'}, lambda r: r['A'])
-    'B'
-    """
-    if isinstance(field, str):
-        return record.__getattribute__(field)
-    elif isinstance(field, int):
-        return record[field]
-    elif hasattr(field, "__call__"):
-        return field(record)
-    else:
-        raise ValueError("Could not locate field %s in %s", str(field), str(record))
 
-def multivalue(sep, *fields):
-    """Return a function that combines delimited fields as a virtual
-    multi-value field.
+class Field(object):
+    """Computes the similarity of a pair of records on a specific field.
+    
+    :type compare: callable(`V`, `V`) :class:`float`
+    :param compare: Returns similarity of a pair of encoded field values.
 
-    :type sep: :class:`str`
-    :param sep: Delimiter to split fields into multiple values
-    :type fields: Field specifiers for :func:`getvalue`
-    :param fields: Fields whose values are to be combined.
-    :rtype: [`V`, ...]
-    :return: The virtual-field values
+    :type field1: callable(`R`) -> `T1`
+    :param field1: Gets field value from first record.
+    :type encode1: callable(`T1`) `V`
+    :param encode1: Encodes field value from first record (default=`lambda x:x`)
+    
+    :type field2: callable(`R`) -> `T1`
+    :param field2: Gets field value from the second record (default=`field1`)
+    :type encode2: callable(`T1`) `V`
+    :param encode2: Encodes field value from the second record (default=`encode1`)
 
     >>> from dedupe import sim
-    >>> sim.multivalue(";", 0, 1)(['1;2;3','4;5'])
-    ['1', '2', '3', '4', '5']
-    """
-    def field_splitter(record):
-        """Create a multi-value from multiple delimited fields %s using delimiter %s"""
-        result = []
-        for field in fields:
-            value = getvalue(record, field)
-            values = [value] if sep is None else value.split(sep)
-            result += [s.strip() for s in values if s.strip()]
-        return result
-    field_splitter.__doc__ %= ",".join([str(x) for x in fields]), sep
-    return field_splitter
-
-def combine(*fields):
-    """Return a function that combines single-value fields into a virtual
-    multi-valued field.
-    
-    :type fields: Field specifiers for :func:`getvalue`
-    :param fields: Fields whose values are to be combined.
-    :rtype: [`V`, ...]
-    :return: The virtual-field values
-    
-    >>> from dedupe import sim
-    >>> sim.combine(0, 2, 3)(['A','B','C','D','E'])
-    ['A', 'C', 'D']
-    """
-    return multivalue(None, *fields)    
-
-
-class ValueSim(object):
-    """Defines a callable comparison of a pair of records on a defined field.
-    
-    :type comparevalues: function(`V`, `V`) :class:`float`
-    :param comparevalues: Compares the encoded values and returns similarity.
-    :type field1: :class:`str`, :class:`int`, or function(`R`)
-    :param field1: the :func:`getvalue` specifier for the first record 
-    :type encode1: function(`T1`) `V`
-    :param encode1: Encoder of field1 values.
-    :type field2: :class:`str`, :class:`int`, or function(`R`)
-    :param field2: the :func:`getvalue` specifier for the second record 
-    :type encode2: function(`T2`) `V`
-    :param encode2: Encoder of field2 value (default=`encode1`).
-    :rtype: callable(`R1`,`R2`) :class:`float`
-    :return: Computes similarity of records `R1` and `R2` on the defined field.
-
-    >>> from dedupe.sim import ValueSim
+    >>> from dedupe.get import item
     >>> # define some 'similarity of numbers' measure
-    >>> similarity = lambda x,y: 2.0**(-abs(x-y))
-    >>> ValueSim(similarity, 1, float)(('A',1),('B',1))
-    1.0
-    >>> ValueSim(similarity, 1, float)(('A','1'),('B','2'))
+    >>> similarity = lambda x,y: 2**-abs(x-y)
+    >>> similarity(1,2)
     0.5
-    >>> ValueSim(similarity, 0, None, 1, float)((1,'A'),('B','2'))
+    >>> Field(similarity, item(1), float)(('A','1'),('B','2'))
+    0.5
+    >>> fsim = Field(similarity, field1=item(0), encode1=lambda x:x, field2=item(1), encode2=float)
+    >>> fsim((1,'A'),('B','2'))
     0.5
     """
     
-    def __init__(self, comparevalues, field1, encode1=None, field2=None, encode2=None):
-        self.comparevalues = comparevalues
-        self.field1 = field1
+    def __init__(self, compare, field1, encode1=None, field2=None, encode2=None):
+        from dedupe.get import getter 
+        self.compare = compare
+        self.field1 = getter(field1)
         self.encode1 = encode1 if encode1 else lambda x:x
-        self.field2 = field2 or field1
+        self.field2 = getter(field2) if field2 else self.field1
         self.encode2 = encode2 if encode2 else self.encode1
 
     def __call__(self, record1, record2):
-        """Compare the two records on the defined fields."""
-        return self.comparevalues(
-            self.encode1(getvalue(record1, self.field1)), 
-            self.encode2(getvalue(record2, self.field2)))
+        """Returns the similarity of `record1` and `record2` on this field."""
+        return self.compare(
+            self.encode1(self.field1(record1)), 
+            self.encode2(self.field2(record2)))
 
+
+class Average(Field):
+    """Computes the average similarity of a pair of records on 
+    a multi-valued field.
+      
+    It computes the average by considering each field value from
+    the record with the fewest values and accumulating the
+    greatest similarity against the values in the other record. 
+    If the shorter field is a subset of the longer field, 
+    the similarity should be 1.0.
     
-class ValueSimAvg(ValueSim):
-    """Return average similarity of multi-valued fields.
-    
-    The average is obtained by looping over the shorter field, and totalling
-    the best similaritois to values of the longer field, then dividing by
-    the length of the shorter field.  That means that if the shorter field
-    is a subset of the longer field, the similarity should work out to 1.0.
-    
-    :type comparevalues: function(`V`, `V`) :class:`float`
-    :param comparevalues: Compares the encoded values and returns similarity.
+    :type compare: callable(`V`, `V`) :class:`float`
+    :param compare: Returns similarity of a pair of encoded field values.
     :type field1: function(`R`) [`T1`,...]
     :param field1: Iterates over multi-values of the field.
     :type field2: function(`R`) [`T2`,...]
     :param field2: Iterates over multi-values of the field (default=`field1`).
     :type encode1: function(`T1`) `V`
-    :param encode1: Encoder of field1 values.
+    :param encode1: Encode of field1 values.
     :type encode2: function(`T2`) `V`
     :param encode2: Encoder of field2 value (default=`encode1`).
     :rtype: callable(`R1`, `R2`) float
@@ -211,109 +137,105 @@ class ValueSimAvg(ValueSim):
     >>> # define an exponential 'similarity of numbers' measure
     >>> similarity = lambda x,y: 2.0**(-abs(x-y))
     >>> field = lambda r: set([r[0],r[2]])
-    >>> from dedupe.sim import ValueSimAvg
-    >>> ValueSimAvg(similarity, field, float)((1,'A','1'),(-2,'B',2))
+    >>> from dedupe import sim
+    >>> sim.Average(similarity, field, float)((1,'A','1'),(-2,'B',2))
     0.5
     >>> field = lambda r: set(r[1].split(';'))
-    >>> ValueSimAvg(similarity, field, float)(('A','0;1'),('B','1;2'))
+    >>> sim.Average(similarity, field, float)(('A','0;1'),('B','1;2'))
     0.75
-    >>> ValueSimAvg(similarity, field, float)(('A','0;1;2'),('B','0;1;2;3;4'))
+    >>> sim.Average(similarity, field, float)(('A','0;1;2'),('B','0;1;2;3;4'))
     1.0
     """
     
     def __call__(self, record1, record2):
-        """Compare two records on a set-of-values field."""
+        """Return the average similarity of `record1` and `record2` on 
+        this multi-valued field"""
         f1 = set(self.encode1(v1) for v1 in self.field1(record1))
         f2 = set(self.encode2(v2) for v2 in self.field2(record2))
         f1, f2 = sorted([f1, f2], key=len) # short set, long set
         # Missing value check
         if len(f1) == 0 or len(f2) == 0:
-            return self.comparevalues(None,None) 
+            return self.compare(None,None) 
         total = 0.0
         for v1 in f1:
             best = 0.0
             for v2 in f2:
-                comp = self.comparevalues(v1,v2)
+                comp = self.compare(v1,v2)
                 best = max(best, comp)
             total += best # score of most similar item in the long set
         return total / len(f1)
 
 
-class ValueSimMax(ValueSim):
-    """Compare the similarity of two sets of values. Set of values obtained 
-    either from splitting a single column, or combining multiple columns.
+class Maximum(Field):
+    """Computes the maximum similarity of a pair of records on a 
+    multi-valued field.
     
-    The similarity is the maximum of the pair-wise comparisons between the two
-    sets. One perfectly matching pair of values between the two sets returns a
-    similarity of 1.0.
-    
-    :type comparevalues: function(`V`, `V`) :class:`float`
-    :param comparevalues: Compares the encoded values and returns similarity.
-    :type field1: function(`R`) [`T1`,...]
-    :param field1: Iterates over multi-values of the field.
-    :type field2: function(`R`) [`T2`,...]
-    :param field2: Iterates over multi-values of the field (default=`field1`).
+    :type compare: callable(`V`, `V`) :class:`float`
+    :param compare: Returns similarity of a pair of encoded field values.
+    :type field1: callable(`R`) [`T1`,...]
+    :param field1: Returns a list of values for the field.
+    :type field2: callable(`R`) [`T2`,...]
+    :param field2: Returns a list of values for the field. (default=`field1`).
     :type encode1: function(`T1`) `V`
-    :param encode1: Encoder of field1 values.
+    :param encode1: Encodes each field1 value for comparison.
     :type encode2: function(`T2`) `V`
-    :param encode2: Encoder of field2 value (default=`encode1`).
-    :rtype: callable(`R1`, `R2`) float
-    :return: Computes maximum similarity of `R1` and `R2` over values of the field.
+    :param encode2: Encodes each field1 value for comparison.(default=`encode1`).
 
     >>> # define an exponential 'similarity of numbers' measure
     >>> similarity = lambda x,y: 2.0**(-abs(x-y))
     >>> field = lambda r: set([r[0],r[2]])
-    >>> from dedupe.sim import ValueSimMax
-    >>> ValueSimMax(similarity, field, float)((0,'A','1'),(2,'B',2))
+    >>> from dedupe import sim
+    >>> sim.Maximum(similarity, field, float)((0,'A','1'),(2,'B',2))
     0.5
     >>> field = lambda r: set(r[1].split(';'))
-    >>> ValueSimMax(similarity, field, float)(('A','0;1;2'),('B','3;4;5'))
+    >>> sim.Maximum(similarity, field, float)(('A','0;1;2'),('B','3;4;5'))
     0.5
     """
     
     def __call__(self, record1, record2):
-        """Compare two records on a set-of-values field."""
+        """Return the maximum similarity of `record1` and `record2` on
+        this multi-valued field."""
         f1 = set(self.encode1(v1) for v1 in self.field1(record1))
         f2 = set(self.encode2(v2) for v2 in self.field2(record2))
         # Missing value check
         if len(f1) == 0 or len(f2) == 0:
-            return self.comparevalues(None,None) 
+            return self.compare(None,None) 
         best = 0.0
         for v1 in f1:
             for v2 in f2:
-                comp = self.comparevalues(v1,v2)
+                comp = self.compare(v1,v2)
                 best = max(best, comp)
         return best
-    
-class RecordSim(_OrderedDict):
+
+
+class Record(_OrderedDict):
     """Returns a vector of field value similarities between two records.
 
-    :type \*simfuncs: [(:class:`str`, :class:`ValueSim`), ...]
-    :param \*simfuncs: Named and ordered field similarity functions.
+    :type \*simfuncs: [(:class:`str`, :class:`Field`), ...]
+    :param \*simfuncs: Pairs of (field name, similarity function) used
+    to compute the tuple of similarities.
     
-    :type Weights: :class:`namedtuple` or (float,...)
-    :ivar Weights: type of similarity vector between records\
-      with field names corresponding to the names in `comparators`.
+    :ivar Similarity: namedtuple class for the similarity of a pair of records,
+    with field names corresponding to `simfuncs`.
     
     :rtype: function(`R`, `R`) :class:`Similarity` 
-    :return: Function that takes two records and returns
-    a `Similarity` tuple of similarity values.
+    :return: Takes two records and returns a `Similarity` tuple.
 
     >>> # define a 'similarity of numbers' measure
     >>> similarity = lambda x,y: 2.0**(-abs(x-y))
-    >>> from dedupe.sim import ValueSim, RecordSim
-    >>> vcomp1 = ValueSim(similarity, 1, float) # field 1 from record
-    >>> vcomp2 = ValueSim(similarity, 2, float) # field 2 from field
-    >>> rcomp = RecordSim(("V1",vcomp1),("V2",vcomp2))
+    >>> from dedupe import sim 
+    >>> vcomp1 = sim.Field(similarity, 1, float) # field 1 from record
+    >>> vcomp2 = sim.Field(similarity, 2, float) # field 2 from field
+    >>> rcomp = sim.Record(("V1",vcomp1),("V2",vcomp2))
     >>> rcomp(('A',1,1), ('B',2,4))
     Similarity(V1=0.5, V2=0.125)
     """
     
     def __init__(self, *simfuncs):
-        super(RecordSim, self).__init__(simfuncs)
+        super(Record, self).__init__(simfuncs)
         import dedupe.compat as c
-        self.Weights = c.namedtuple("Similarity", self.keys())
+        self.Similarity = c.namedtuple("Similarity", self.keys())
 
     def __call__(self, A, B):
-        return self.Weights._make(
-            valuesim(A, B) for valuesim in self.itervalues())
+        return self.Similarity._make(
+            fieldsimilarity(A, B) for fieldsimilarity in self.itervalues())
