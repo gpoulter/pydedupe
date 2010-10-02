bae9636ea0b7aa601eac1964ac387e4a379ebf12
Graham Poulter <graham@mocality.com>
2010-10-02 16:04:48 +0200

    No need for crufty febrl code

--- dedupe/febrl/__init__.py
+++
@@ -1,3 +0,0 @@
-"""FEBRL is the Freely Extensible Biomedical Record Linkage program.  Its
-classification and comparison modules have been included in PyDedupe to
-provide subroutines for comparing strings and classifying vectors."""
